83ea7d9dad82849769e40d1674e9c717f569b4cf
AsadUllahShaikh123 <asadullahshaikh713@gmail.com>
2022-01-14 23:30:45 +0500

    Added Image

---
+++ src/components/Projects.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ImageComponent from './ImageComponent'
+import SubComponent from './SubComponent'
+function Projects() {
+  return (
+    <div className='mt-16'>
+      <div className='flex flex-col  items-center w-full'>
+        <p className='text-5xl font-mono mb-2 '>PRO<span className='text-red-600'>J</span>ECTS</p>
+        <div className='h-2 w-1/6 bg-gray-500 rounded-full mb-10'>
+
+        </div>
+      </div>
+      <div className='w-full flex justify-center'>
+        <div className='border-2 border-gray-500 w-2/3 '>
+          <div className='grid grid-cols-2 gap-0'>
+            <SubComponent/>
+            <ImageComponent/>
+            <ImageComponent/>
+            <SubComponent/>            
+            <SubComponent/>    
+            <ImageComponent/>
+        
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
